49ff0c04c9ca1a667c583c430684ef9c8fd14c34
OctavianaSholikhah <112896620+octaviana0003@users.noreply.github.com>
2022-09-09 10:54:49 +0700

    Add files via upload

---
+++ Tugas 1 daspro/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Octaviana Sholikhah \nNIM : A11.2022.14640 \nAsal Sekolah : SMA NEGERI 2 SAROLANGUN" << endl;
+    return 0;
+}
