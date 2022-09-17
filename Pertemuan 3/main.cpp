7cb35c2743e2ed3c186fe2c21d174a6e1e8e7096
OctavianaSholikhah <112896620+octaviana0003@users.noreply.github.com>
2022-09-17 19:27:48 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 14640;
+    cout << "Octaviana Sholikhah " << x;
+    /* comment
+    */
+
+    int u = 18;
+    float t = 160;
+    char b = 'L';
+    string ub = "XL";
+
+    cout << "\nUmur saya:"<< u;
+    cout << endl;
+    cout << "Tinggi badan:"<< t;
+    cout << "\nUkuran baju:"<< ub;
+    return 0;
+}
